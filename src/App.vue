62acc4124d7fc1e2ee8b12aa69d0d5fcc2964330
Pedro Corado <pedrocorado2025@gmail.com>
2023-11-08 21:28:16 -0300

    Calculadora com Vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    <Calculadora />
+  </div>
+</template>
+
+<script>
+import Calculadora from "@/Calculadora.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calculadora,
+  },
+};
+</script>
---
+++ src/calculadora.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <input v-model="numero1" type="number" @input="calcularResultado" />
+      <select v-model="operacao" @change="calcularResultado">
+        <option value="soma">+</option>
+        <option value="subtracao">-</option>
+        <option value="multiplicacao">*</option>
+        <option value="divisao">/</option>
+      </select>
+      <input v-model="numero2" type="number" @input="calcularResultado" />
+      <p>Resultado: {{ resultado }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        numero1: 0,
+        numero2: 0,
+        operacao: "soma",
+        resultado: 0,
+      };
+    },
+    methods: {
+      calcularResultado() {
+        if (this.operacao === "soma") {
+          this.resultado = this.numero1 + this.numero2;
+        } else if (this.operacao === "subtracao") {
+          this.resultado = this.numero1 - this.numero2;
+        } else if (this.operacao === "multiplicacao") {
+          this.resultado = this.numero1 * this.numero2;
+        } else if (this.operacao === "divisao") {
+          this.resultado = this.numero1 / this.numero2;
+        }
+      },
+    },
+  };
+  </script>
+  
